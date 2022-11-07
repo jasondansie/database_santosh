b0e0e91dd1226be3a4b135d529b4fc4953dbaad7
Jason Dansie <jasondansie@gmail.com>
2022-11-07 14:40:13 +0200

    adds solutions to these examples

--- Practice_0411/productOfArray.js
+++ Practice_0411/productOfArray.js
@@ -4,9 +4,16 @@
 // HINT: array method e.g. slice
 
 function productOfArray(arr) {
-
+    if (arr.length == 0) {
+        return 1;     
+    }
+    return arr[0] * productOfArray(arr.slice(1));
 }
 
 
 // productOfArray([1,2,3]) // 6
 // productOfArray([1,2,3,10]) // 60
+
+console.log(productOfArray([1,2,3])); //6
+console.log(productOfArray([1,2,3,10])); //60
+console.log(productOfArray([])); //1
--- Practice_0411/recursiveRange.js
+++ Practice_0411/recursiveRange.js
@@ -2,9 +2,17 @@
 //and adds up all the numbers from 0 to the number passed to the func,on
 
 function recursiveRange(x){
-
-  
+    let num = 0;
+    if (x == 0) {
+        return num;
+    }
+    else{
+        num = x + recursiveRange(x-1);
+        return num;
+    }
 }
 
 // recursiveRange(6) // 21
 // recursiveRange(10) // 55
+console.log(recursiveRange(6)); //21
+console.log(recursiveRange(10)); //55
