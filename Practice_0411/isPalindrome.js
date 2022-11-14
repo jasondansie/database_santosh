5d275c7b16e8b4043a77df15d462035416e60560
Jason Dansie <jasondansie@gmail.com>
2022-11-14 14:41:30 +0200

    adds solution to palindrome problem

--- Practice_0411/isPalindrome.js
+++ Practice_0411/isPalindrome.js
@@ -3,5 +3,14 @@
 //Otherwise it returns false.
 
 function isPalindrome(str){
-  
+    if (str.length == 1 || str.length == 0) {
+        return true;  
+    }
+    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+    return false;
 }
+
+console.log(isPalindrome("racecar")); //true
+console.log(isPalindrome("dog")); //false
+console.log(isPalindrome("")); //true
+console.log(isPalindrome("a")); //true
