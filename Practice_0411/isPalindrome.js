44c57db613d9ac48cba0debd2fa49ae2d40d7e2f
Jason Dansie <jasondansie@gmail.com>
2022-11-21 10:02:11 +0200

    Updates js

--- Practice_0411/isPalindrome.js
+++ Practice_0411/isPalindrome.js
@@ -6,8 +6,9 @@ function isPalindrome(str){
     if (str.length == 1 || str.length == 0) {
         return true;  
     }
-    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
-    return false;
+    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1)){
+        return false;
+    }     
 }
 
 console.log(isPalindrome("racecar")); //true
